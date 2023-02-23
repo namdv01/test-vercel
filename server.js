ead8a6f2a904b7634a3ed3dd920792f5c5b2990e
namdv01 <khonghoatay@gmail.com>
2023-02-23 20:25:13 +0700

    t1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fastify = require('fastify')();
 fastify.get('/', (request, reply) => {
   reply.send({ hello: 'world' })
 })
-fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+fastify.listen(3000, (err, address) => {
   if (err) throw err
   // Server is now listening on ${address}
 }) 
